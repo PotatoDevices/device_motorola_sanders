2f6fac66ac0a3c5f530287784ac636c220107b86
Vachounet <vachounet@live.fr>
2018-02-04 17:18:14 +0100

    sanders: add health HIDL

--- device.mk
+++ device.mk
@@ -194,6 +194,7 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
