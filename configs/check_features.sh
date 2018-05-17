98abfcea1dc46991275514a708ec902057a17dbb
Nick <nickvanbokhorst@hotmail.com>
2018-05-17 16:07:27 +0200

    sanders: Fix path of check_features.sh (#17)
    *Also correct the path of the nfc, compass & DTV files

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -10,21 +10,21 @@ sku=`getprop ro.boot.hardware.sku`
 
 if [ "$sku" = "XT1806" ]; then
     # XT1806 doesn't have NFC chip
-    rm /system/vendor/etc/permissions/android.hardware.nfc.xml
-    rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
-    rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
 fi
 
 if ! [ "$sku" = "XT1802" ]; then
     # Others variants doesn't have DTV support
-    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
-    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
-    rm /system/vendor/lib/libdtvtuner.so
-    rm /system/vendor/lib64/libdtvtuner.so
-    rm /system/vendor/lib/libdtvhal.so
-    rm /system/vendor/lib64/libdtvhal.so
-    rm -r /system/vendor/app/DTVPlayer
-    rm -r /system/vendor/app/DTVService
+    rm /vendor/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /vendor/etc/permissions/mot_dtv_permissions.xml
+    rm /vendor/lib/libdtvtuner.so
+    rm /vendor/lib64/libdtvtuner.so
+    rm /vendor/lib/libdtvhal.so
+    rm /vendor/lib64/libdtvhal.so
+    rm -r /vendor/app/DTVPlayer
+    rm -r /vendor/app/DTVService
 fi
 
