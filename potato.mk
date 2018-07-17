ab5349029bd322c575709b006f87282e0da4bce7
Keertesh19 <keertesh.k98@gmail.com>
2018-07-17 13:18:07 +0000

    Potatoes FTW!

--- potato.mk
+++ potato.mk
@@ -18,15 +18,16 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 
-# Inherit some common AospExtended stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some Tasty Potatoes stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := aosp_sanders
+PRODUCT_NAME := potato_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
