4f58f0144d15d6c00eed0a0511d561fbb3cdfabd
Prema Chand Alugu <premaca@gmail.com>
2018-08-18 15:50:51 +0530

    sanders: Consider gzosp_sanders product via the AOSP way

--- gzosp_sanders.mk
+++ gzosp_sanders.mk
@@ -1,22 +1,25 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/motorola/sanders/full_sanders.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from potter device
+$(call inherit-product, device/motorola/sanders/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
@@ -36,5 +39,5 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto G5S Plus"
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Prema Chand Alugu (premaca), Adam Ovadia (oadam11)"
+# for specific
+$(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
