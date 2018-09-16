d18f8301711a6b3a90a062a1d24b84eb113c149d
erfanoabdi <erfangplus@gmail.com>
2018-09-16 11:56:26 +0000

    sanders: Convert libinit to init scripts
     * Also set ro.product.manufacturer to motorola for camera HAL in GSIs.
    
    Change-Id: I2c78e4837cdbd8e7f18e3c1a086978bd289a60a6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,10 +61,6 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 # HDIL - enable this for ROMs build base/transport hidl in their sources
 #BUILD_DEVICE_LIBHIDL := false
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_sanders
-TARGET_RECOVERY_DEVICE_MODULES := libinit_sanders
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M androidboot.selinux=permissive
