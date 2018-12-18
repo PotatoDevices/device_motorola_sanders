11b1f1be69906d605ba2ce8a0cc71fa9e2033b83
dybios <dibyajyoti.samal@gmail.com>
2018-12-18 06:17:21 +0000

    Update kernel path 	modified:  BoardConfig.mk 	modified:   potato.dependencies

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := sanders_defconfig
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
+TARGET_KERNEL_SOURCE := kernel/motorola/sanders
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
