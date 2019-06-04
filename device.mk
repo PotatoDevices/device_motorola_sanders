a62a417be28a1dd08fc4fa7a10e7468f6a02a9b0
kubersharma001 <kubersharma001@gmail.com>
2019-06-05 04:10:20 +0530

    sanders: Build CarrierConfig

--- device.mk
+++ device.mk
@@ -441,6 +441,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# CarrierConfig
+PRODUCT_PACKAGES += \
+    CarrierConfig
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
