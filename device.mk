33764b3bd41c020d98cf7ca586808e579d390cfc
Alberto97 <albertop2197@gmail.com>
2019-08-14 03:33:06 +0530

    sanders: Pie RIL upgrade

--- device.mk
+++ device.mk
@@ -359,7 +359,8 @@ PRODUCT_PACKAGES += \
 
 #RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
