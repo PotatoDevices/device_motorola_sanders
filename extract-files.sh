c28e2c0a6bf3f87b0e4bdc90e99fd53f28419496
dybios <dibyajyoti.samal@gmail.com>
2019-09-27 14:29:05 +0530

    sanders: Fix helper script path in extract-files
    - Should have been done long back rip

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=motorola
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+POTATO_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$POTATO_ROOT"/vendor/potato/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -50,7 +50,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$POTATO_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
