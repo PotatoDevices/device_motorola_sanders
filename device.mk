1bb95922ffe361e3c1024feb490d85bace6ac161
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-11-07 23:18:01 +0100

    sanders: Uprev Power Hal to 1.2
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- device.mk
+++ device.mk
@@ -302,7 +302,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
