7304c4a9a330af9584f82c7a1893abab0dbac25e
Dybios <dibyajyoti.samal@gmail.com>
2019-12-26 21:37:15 +0100

    sanders: Move to legacy-um sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -227,7 +227,7 @@ LZMA_RAMDISK_TARGETS := recovery
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Thermal
