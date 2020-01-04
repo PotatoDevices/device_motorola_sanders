ada0a31c1cc91b941aa7dbb2229be4d7c13459b7
Dybios <dibyajyoti.samal@gmail.com>
2020-01-04 08:29:47 +0100

    sanders: Increase dex2oat thread count to 8

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -372,7 +372,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # VM properties to make phone actually usable
 # Why does this even work
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.dex2oat_thread_count=4 \
+    ro.sys.fw.dex2oat_thread_count=8 \
     dalvik.vm.boot-dex2oat-threads=8 \
     dalvik.vm.dex2oat-threads=8 \
     dalvik.vm.image-dex2oat-threads=8 \
