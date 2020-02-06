45c927d16206a36f2dae826ea9786fd445301d62
dybios <dibyajyoti.samal@gmail.com>
2020-02-06 21:16:47 +0530

    sanders: Fix and add a few paths for check_features

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -12,8 +12,12 @@ if [ "$sku" = "XT1806" ]; then
     # XT1806 doesn't have NFC chip
     rm /vendor/etc/permissions/android.hardware.nfc.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
     rm /vendor/etc/permissions/com.android.nfc_extras.xml
-    rm -r /system/app/NfcNci
+    rm -r /system/system/app/NfcNci
+else
+    # Only XT1806 variant got a compass
+    rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
 
 if ! [ "$sku" = "XT1802" ]; then
