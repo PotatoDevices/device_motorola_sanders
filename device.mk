ed3244d2442aa22d8d7862070cafd6daa42e020e
kubersharma001 <kubersharma001@gmail.com>
2020-03-02 19:02:03 +0530

    sanders: Build more telephony and radio packages
    Change-Id: I694105dacf9cb0bd62aa0c62f7de1ab72353e195

--- device.mk
+++ device.mk
@@ -220,6 +220,11 @@ PRODUCT_COPY_FILES += \
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
@@ -369,15 +374,13 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0 \
     librmnetctl \
     libprotobuf-cpp-full \
     libprotobuf-cpp-lite \
-    libxml2
-
-#RIL
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio.config@1.0
+    libxml2 \
+    libcnefeatureconfig
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
