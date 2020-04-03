880f7811ed68c9a127f2a471979fe5ac2a02a41a
Dybios <dibyajyoti.samal@gmail.com>
2020-04-03 21:17:20 +0200

    sanders: Switch to CAF surfaceflinger
    Move to product configs instead of board.
    Clean up the spaces a little while we're at it.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -252,9 +252,6 @@ TARGET_USES_MKE2FS := true
 # Encryption
 TARGET_KEYMASTER_SKIP_WAITING_FOR_QSEE := true
 
-# SurfaceFlinger
-TARGET_USE_QCOM_SURFACEFLINGER := true
-
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_WLAN_DEVICE                := qcwcn
