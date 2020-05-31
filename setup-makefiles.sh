dbdc3140fd90ecc9c50ee5fbdf4980c8aecb6ddc
AshwinRC <ashwin2001achu@gmail.com>
2020-05-31 16:50:44 +0200

    sanders: Fix setup makefile script
    Enable treble makefile support
    Fix execute perms for both scripts

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,9 +25,9 @@ VENDOR=motorola
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+POTATO_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$POTATO_ROOT"/vendor/potato/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -35,13 +35,13 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$POTATO_ROOT"
 
 # Copyright headers and guards
 write_headers
 
 # The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
+write_makefiles "$MY_DIR"/proprietary-files.txt true
 
 # We are done!
 write_footers
