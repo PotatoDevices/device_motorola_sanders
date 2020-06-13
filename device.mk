48f52a52e2f83220dd3ee774f69ff84b8f5d1f31
Dybios <dibyajyoti.samal@gmail.com>
2020-06-13 22:33:39 +0200

    sanders: Add QPerformance and UxPerformance jars for boost framework
    Signed-off-by: Dybios <dibyajyoti.samal@gmail.com>

--- device.mk
+++ device.mk
@@ -330,6 +330,11 @@ PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml \
      $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
+# Perf Jars
+PRODUCT_BOOT_JARS += \
+   QPerformance \
+   UxPerformance
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
