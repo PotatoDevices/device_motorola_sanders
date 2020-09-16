77ad84861b9c8fde710d735cb1a5e7c9c6271bbc
Dybios <dibyajyoti.samal@gmail.com>
2020-09-17 01:22:28 +0200

    sanders: Nuke releasetools for now
    Segfaults TWRP giving error 11.
    Hence, nuke for now.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,9 +222,6 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 LZMA_RAMDISK_TARGETS := recovery
 
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
-
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
