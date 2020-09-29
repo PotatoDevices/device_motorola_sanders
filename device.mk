93983d0133422ef8b8c87678006494a2a09a33c8
Dybios <dibyajyoti.samal@gmail.com>
2020-09-29 20:54:43 +0200

    [TEMP]sanders: Use Power HAL 1.0

--- device.mk
+++ device.mk
@@ -322,7 +322,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
+    power.qcom
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
