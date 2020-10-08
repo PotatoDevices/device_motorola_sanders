fec3c594e1447a3d572ffc926ecf1efe9413a022
Dybios <dibyajyoti.samal@gmail.com>
2020-10-08 09:46:54 +0200

    sanders: Build some additional camera packages

--- device.mk
+++ device.mk
@@ -119,8 +119,10 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
