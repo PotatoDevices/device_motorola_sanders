2f0bc07be6aae87dd2bc395adef8a1698734f2ea
Arvind Kumar <karvin@codeaurora.org>
2021-01-01 22:05:25 +0100

    sanders: Add vndservicemanager explicitly
    This is now added to product_packages based on shipping_api which is
    not yet supported on few targets. Hence adding it explicitly.
    
    Change-Id: I4bc6d43627e92e1d7a7d3fe82ec6e58fc5fdaca7

--- device.mk
+++ device.mk
@@ -499,6 +499,9 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
 
+# vndservicemanager
+PRODUCT_PACKAGES += vndservicemanager
+
 PRODUCT_ENFORCE_RRO_TARGETS :=*
 PRODUCT_USE_QCOM_SURFACEFLINGER := true
 PRODUCT_BOARD_PLATFORM := msm8996
